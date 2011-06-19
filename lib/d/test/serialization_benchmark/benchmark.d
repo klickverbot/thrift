cc4534293566c39c7de639245efdd97573032fc6
David Nadlinger <code@klickverbot.at>
2011-06-19 05:01:56 +0200

    Hoist object creation out of the benchmark read loop.

--- lib/d/test/serialization_benchmark/benchmark.d
+++ lib/d/test/serialization_benchmark/benchmark.d
@@ -8,23 +8,22 @@ import thrift.transport.memory;
 import DebugProtoTest_types;
 
 void main() {
-  auto ooe = OneOfEach();
-  ooe.im_true   = true;
-  ooe.im_false  = false;
-  ooe.a_bite    = 0x7f;
-  ooe.integer16 = 27_000;
-  ooe.integer32 = 1 << 24;
-  ooe.integer64 = 6_000_000_000;
-  ooe.double_precision = PI;
-  ooe.some_characters = "JSON THIS! \"\1";
-  ooe.zomg_unicode = "\xd7\n\a\t";
-  ooe.base64 = "\1\2\3\255";
-
   auto buf = new TMemoryBuffer;
-
   enum ITERATIONS = 1_000_000;
 
   {
+    auto ooe = OneOfEach();
+    ooe.im_true   = true;
+    ooe.im_false  = false;
+    ooe.a_bite    = 0x7f;
+    ooe.integer16 = 27_000;
+    ooe.integer32 = 1 << 24;
+    ooe.integer64 = 6_000_000_000;
+    ooe.double_precision = PI;
+    ooe.some_characters = "JSON THIS! \"\1";
+    ooe.zomg_unicode = "\xd7\n\a\t";
+    ooe.base64 = "\1\2\3\255";
+
     auto sw = StopWatch(AutoStart.yes);
     auto prot = createTBinaryProtocol(buf);
     foreach (i; 0 .. ITERATIONS) {
@@ -40,12 +39,14 @@ void main() {
   auto data = buf.getContents();
 
   {
+    auto readBuf = new TMemoryBuffer();
+    auto prot = createTBinaryProtocol(readBuf);
+    auto ooe = OneOfEach();
+
     auto sw = StopWatch(AutoStart.yes);
     foreach (i; 0 .. ITERATIONS) {
-      auto buf2 = new TMemoryBuffer(data);
-      auto prot = createTBinaryProtocol(buf2);
-      auto ooe2 = OneOfEach();
-      ooe2.read(prot);
+      readBuf.write(data);
+      ooe.read(prot);
     }
     sw.stop();
 
