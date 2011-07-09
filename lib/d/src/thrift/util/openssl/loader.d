5890f19225ef13e6d2dd1d63838c749a9e05ecb2
David Nadlinger <code@klickverbot.at>
2011-07-09 23:35:15 +0200

    Factored general loading code out of SSL bindings.

--- lib/d/src/thrift/util/openssl/loader.d
+++ lib/d/src/thrift/util/openssl/loader.d
@@ -1,19 +1,9 @@
-/**
- * Support code for dynamically loading shared libraries, adapted for loading
- * OpenSSL.
- *
- * The loading code was adapted from the Derelict project and is used with
- * permission from Michael Parker, the original author.
- *
- * Author: David Nadlinger
- *
- * License: Public Domain (Boost Software License 1.0 where not applicable).
- */
 module thrift.util.openssl.loader;
 
 import std.conv : to;
 import std.string : toStringz;
 import std.traits : isFunctionPointer;
+import thrift.util.loader;
 
 enum Library {
   crypto,
@@ -60,134 +50,6 @@ shared static this() {
   }
 }
 
-class SharedLibLoadException : Exception {
-  this(in string[] libNames, in string[] reasons, string file = __FILE__,
-    size_t line = __LINE__, Throwable next = null)
-  {
-    string msg = "Failed to load one or more shared libraries:";
-    foreach(i, n; libNames) {
-      msg ~= "\n\t" ~ n ~ " - ";
-      if(i < reasons.length)
-        msg ~= reasons[i];
-      else
-        msg ~= "Unknown";
-    }
-    super(msg, file, line, next);
-  }
-}
-
-class SymbolLoadException : Exception {
-  this(string sharedLibName, string symbolName, string file = __FILE__,
-    size_t line = __LINE__, Throwable next = null)
-  {
-    super("Failed to load symbol " ~ symbolName ~ " from shared library " ~
-    sharedLibName, file, line, next);
-    symbolName_ = symbolName;
-  }
-
-  string symbolName() {
-    return symbolName_;
-  }
-
-private:
-  string symbolName_;
-}
-
 private {
   __gshared SharedLib[Library.max + 1] g_sharedLibs;
-
-  final class SharedLib {
-    void load(string[] names) {
-      if (hlib_ !is null) return;
-
-      string[] failedLibs;
-      string[] reasons;
-
-      foreach(n; names) {
-        hlib_ = loadSharedLib(n);
-        if (hlib_ is null) {
-          failedLibs ~= n;
-          reasons ~= getErrorStr();
-          continue;
-        }
-        name_ = n;
-        break;
-      }
-
-      if (hlib_ is null) {
-        throw new SharedLibLoadException(failedLibs, reasons);
-      }
-    }
-
-    void* loadSymbol(string symbolName, bool doThrow = true) {
-      void* sym = getSymbol(hlib_, symbolName);
-      if(doThrow && (sym is null)) {
-        throw new SymbolLoadException(name_, symbolName);
-      }
-      return sym;
-    }
-
-    void unload() {
-      if(hlib_ !is null) {
-        unloadSharedLib(hlib_);
-        hlib_ = null;
-      }
-    }
-
-  private:
-    string name_;
-    SharedLibHandle hlib_;
-  }
-
-  version (Posix) {
-    version (freebsd) {} else {
-      pragma(lib, "dl");
-    }
-
-    import core.sys.posix.dlfcn;
-    alias void* SharedLibHandle;
-
-    SharedLibHandle loadSharedLib(string libName) {
-      return dlopen(toStringz(libName), RTLD_NOW);
-    }
-
-    void unloadSharedLib(SharedLibHandle hlib) {
-      dlclose(hlib);
-    }
-
-    void* getSymbol(SharedLibHandle hlib, string symbolName) {
-      return dlsym(hlib, toStringz(symbolName));
-    }
-
-    string getErrorStr() {
-      auto err = dlerror();
-      if (err is null) {
-        return "Unknown Error";
-      }
-      return to!string(err);
-    }
-  } else version (Windows) {
-    import core.sys.windows.windows;
-    import std.windows.syserror;
-
-    alias HMODULE SharedLibHandle;
-
-    SharedLibHandle loadSharedLib(string libName) {
-      return LoadLibraryA(toStringz(libName));
-    }
-
-    void unloadSharedLib(SharedLibHandle hlib) {
-      FreeLibrary(hlib);
-    }
-
-    void* getSymbol(SharedLibHandle hlib, string symbolName) {
-      return GetProcAddress(hlib, toStringz(symbolName));
-    }
-
-    string getErrorStr() {
-      return sysErrorString(GetLastError());
-    }
-  } else {
-    static assert(false, "OS not supported by the library loading code.");
-  }
 }
