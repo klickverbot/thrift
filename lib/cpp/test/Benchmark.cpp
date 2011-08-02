7b1704cb241658d6714f22eb72243e0df06b6f0e
David Nadlinger <code@klickverbot.at>
2011-08-02 17:42:22 +0200

    Moved struct construction back into the benchmark loop.
    Previously, the std::string memory buffers were reused, which is both unrealistic, and gave an unfair advantage over e.g. D the implementation.

--- lib/cpp/test/Benchmark.cpp
+++ lib/cpp/test/Benchmark.cpp
@@ -87,11 +87,11 @@ int main() {
 
   {
     Timer timer;
-    OneOfEach ooe2;
     shared_ptr<TMemoryBuffer> buf2(new TMemoryBuffer(data, datasize));
     TBinaryProtocolT<TBufferBase> prot(buf2);
 
     for (int i = 0; i < num; i ++) {
+      OneOfEach ooe2;
       buf2->resetBuffer(data, datasize);
       ooe2.read(&prot);
     }
